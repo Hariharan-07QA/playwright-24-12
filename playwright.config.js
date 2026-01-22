2c8b94428d20f190ed14587e9174f79e359362a5
Hariharan-07QA <hariselvakumar9080@gmail.com>
2026-01-22 10:51:32 +0530

    commit these 4 files

--- playwright.config.js
+++ playwright.config.js
@@ -23,7 +23,7 @@ module.exports = defineConfig({
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
     ['list'],
-    ['allure-playwright']
+    ['allure-playwright',{outputFolder:'allure-results'}]
   ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
